e6d437d056591f49e5bb4b394626d3b926165533
hjsuh18@gmail.com <hjsuh18@gmail.com>
2020-12-10 01:18:38 +0000

    serialization markdown

---
+++ serialization.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC Commands got padded with newlines
+# MAGIC 
+# MAGIC Whitespace after MAGIC command
+# MAGIC 
+# MAGIC Whitespace removed in the end of the notebooks
+# MAGIC 
+# MAGIC One line between COMMAND becomes three new lines
+
+# COMMAND ----------
+
