5443d408eaae1db69fccc609cc6444ea7eea6233
HJ Suh <hj.suh@databricks.com>
2020-12-10 14:32:40 -0800

    edit after divider and preamble

---
+++ edit_after_divider.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+print("hello world")
+
+# COMMAND edit after divider
+
+1 + 1
+
+# COMMAND ----------
+
+2 + 2
+
+# COMMAND ----------
+
+3 + 3
+
+# COMMAND ----------
---
+++ edit_after_preamble.py
@@ -0,0 +1,16 @@
+# Databricks notebook source edit after preamble
+print("hello world")
+
+# COMMAND ----------
+
+1 + 1
+
+# COMMAND ----------
+
+2 + 2
+
+# COMMAND ----------
+
+3 + 3
+
+# COMMAND ----------
