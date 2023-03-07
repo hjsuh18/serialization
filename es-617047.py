4259a8c42c984f61f2f7853f1b6bbc412ad7ff5d
hjsuh18 <hjsuh18>
2023-03-07 21:59:06 +0000

    create notebook for ES ticket

---
+++ es-617047.py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Hi this is some markdown
+# MAGIC This is text
+
+# COMMAND ----------
+
+# DBTITLE 1,This is a DBTITLE title
+print("this is a cell with a title")
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
