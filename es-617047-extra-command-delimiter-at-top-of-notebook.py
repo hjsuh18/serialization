c61072d8e3b2ae02dfda278da500ba843123e03e
hj.suh <hj.suh@databricks.com>
2023-03-07 17:35:45 -0800

    extra command delimiter at top of notebook

---
+++ es-617047-extra-command-delimiter-at-top-of-notebook.py
@@ -0,0 +1,26 @@
+# Databricks notebook source
+# COMMAND ----------
+
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Hi this is some markdown
+# MAGIC 
+# MAGIC This is text
+# MAGIC 
+# MAGIC This is more text
+
+# COMMAND ----------
+
+# DBTITLE 1,This is a DBTITLE title
+print("this is a cell with a title")
+
+# COMMAND ----------
+
+print("this is a cell without a newline after COMMAND divider")
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
