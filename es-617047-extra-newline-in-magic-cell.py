ef0b12dde773846bb96421424945cbaab6b8c6e6
hj.suh <hj.suh@databricks.com>
2023-03-07 17:40:22 -0800

    extra newline in magic cell

---
+++ es-617047-extra-newline-in-magic-cell.py
@@ -0,0 +1,16 @@
+# Databricks notebook source
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Hi this is some markdown
+# MAGIC 
+# MAGIC This is text
+# MAGIC 
+# MAGIC This is more text
+
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
