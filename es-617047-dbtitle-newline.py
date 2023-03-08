543b966f68425b0f535bbad2161c15b7d66d1ee5
hj.suh <hj.suh@databricks.com>
2023-03-07 17:24:09 -0800

    add notebook with dbtitle newline in different place

---
+++ es-617047-dbtitle-newline.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+# DBTITLE 1,This is a DBTITLE title
+
+print("this is a cell with a title")
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
