c65b2505e5284aec189b81a7782b4cbbba4add0d
hj.suh <hj.suh@databricks.com>
2023-03-07 17:32:18 -0800

    DBTITLE moved to top bug

---
+++ es-617047-dbtitle-moved-to-top.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+
+print("this is a command before the DBTITLE magic line")
+
+# DBTITLE 1,This is a DBTITLE title
+print("this is a cell with a title")
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
