cd67275e1acfdb3798ff3e72fd45db144dec0575
hj.suh <hj.suh@databricks.com>
2023-03-07 17:06:49 -0800

    isolate bug with just space after magic removed

---
+++ es-617047-space-after-magic-removed.py
@@ -0,0 +1,15 @@
+# Databricks notebook source
+print("hi this is a notebook that contains bugs reported in es-617047")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Hi this is some markdown
+# MAGIC 
+# MAGIC MAGIC below me does not have a trailing space
+# MAGIC
+# MAGIC I am dummy magic
+
+# COMMAND ----------
+
+print("this is the last cell of the notebook")
